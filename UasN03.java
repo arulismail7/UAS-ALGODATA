cdd03f9db69967e473a9275a1e330a4e62b7b2c5
Sahrul Ismail Usman <79994875+arulismail7@users.noreply.github.com>
2021-07-11 20:37:17 +0700

    Add files via upload

---
+++ UasN03.java
@@ -0,0 +1,17 @@
+public class UasN03{
+    public static void main(String[] args){
+        //Mendefinisikan Objek Beserta Nilainya
+        StringBuffer data = new StringBuffer("Saya sedang belajar struktur data");
+        
+        //Menyisipkan Karakter pada Objek StringBuffer
+        data.insert(12, " ");
+        data.insert(13, "p");
+        data.insert(14, "e");
+        data.insert(15, "r");
+        data.insert(16, "g");
+        data.insert(17, "i");
+        data.insert(18, " ");
+        //Menampilkan Output
+        System.out.println(data);
+    }
+}
